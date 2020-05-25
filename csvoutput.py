e4116b6abac00cd7534c85787cff732f11212a90
Clayton Gravatt <claytonmgravatt@gmail.com>
2020-05-25 15:56:03 -0700

    Added comment

--- csvoutput.py
+++ csvoutput.py
@@ -4,7 +4,9 @@ import csv
 class CsvOutput :
     #This function exports a list to a csv file
     def csvwrite(self, mylist, q) :
+       #generate uniquely named files
         #queryc = q.replace(" ","-")
+       #overwrite existing csv file
         queryc = 'default'
         with open(str(queryc) + '.csv','w', newline='') as myfile:
             wr = csv.writer(myfile, quoting=csv.QUOTE_ALL)
