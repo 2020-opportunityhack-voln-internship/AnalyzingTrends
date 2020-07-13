2665aebc24b4dc1ee83215e540770c2a1c317cd8
Clayton Gravatt <claytonmgravatt@gmail.com>
2020-07-13 09:37:14 -0700

    changing requirements for twitterscraper

---
+++ twitterscraper/setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+from setuptools import setup, find_packages
+with open('requirements.txt') as requirements:
+    required = requirements.read().splitlines()
+
+setup(
+    name='twitterscraper',
+    version='1.4.0',
+    description='Tool for scraping Tweets',
+    url='https://github.com/taspinar/twitterscraper',
+    author=['Ahmet Taspinar', 'Lasse Schuirmann'],
+    author_email='taspinar@gmail.com',
+    license='MIT',
+    packages=find_packages(exclude=["build.*", "tests", "tests.*"]),
+    install_requires=required,
+    entry_points={
+        "console_scripts": [
+            "twitterscraper = twitterscraper.main:main"
+        ]
+    })
