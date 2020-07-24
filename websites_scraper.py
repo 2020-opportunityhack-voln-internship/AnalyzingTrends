9f6a3a72f1a281400b9a304d7e483cd04fdef533
Clayton Gravatt <claytonmgravatt@gmail.com>
2020-07-24 12:02:40 -0700

    trying reduced chrome arguments

--- websites_scraper.py
+++ websites_scraper.py
@@ -11,15 +11,20 @@ class ScrapeFunction:
 
     def setup_chrome():
         chrome_options = Options()
-        chrome_options.add_argument("--window-size=1920,1080")
-        chrome_options.add_argument("--disable-gpu")
-        chrome_options.add_argument("--disable-extensions")
-        chrome_options.add_experimental_option("useAutomationExtension", False)
-        chrome_options.add_argument("--proxy-server='direct://'")
-        chrome_options.add_argument("--proxy-bypass-list=*")
-        chrome_options.add_argument("--start-maximized")
-        chrome_options.add_argument("--no-sandbox")
-        chrome_options.add_argument("--disable-dev-shm-usage")
+        # chrome_options.add_argument("--window-size=1920,1080")
+        # chrome_options.add_argument("--disable-gpu")
+        # chrome_options.add_argument("--disable-extensions")
+        # chrome_options.add_experimental_option("useAutomationExtension", False)
+        # chrome_options.add_argument("--proxy-server='direct://'")
+        # chrome_options.add_argument("--proxy-bypass-list=*")
+        # chrome_options.add_argument("--start-maximized")
+        # chrome_options.add_argument("--no-sandbox")
+        # chrome_options.add_argument("--disable-dev-shm-usage")
+        
+        chrome_options.add_argument('--headless')
+        chrome_options.add_argument('--no-sandbox')
+        chrome_options.add_argument('--disable-dev-shm-usage')
+        
         chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
     
         return chrome_options
