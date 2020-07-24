f08813403efdb081142883730874951d636e74af
Clayton Gravatt <claytonmgravatt@gmail.com>
2020-07-24 09:50:47 -0700

    testing removing headless

--- websites_scraper.py
+++ websites_scraper.py
@@ -11,7 +11,6 @@ class ScrapeFunction:
 
     def setup_chrome():
         chrome_options = Options()
-        chrome_options.add_argument("--headless")
         chrome_options.add_argument("--window-size=1920,1080")
         chrome_options.add_argument("--disable-gpu")
         chrome_options.add_argument("--disable-extensions")
